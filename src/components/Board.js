97a77d550ed8ef64dcf844d5c69ed08d0c443191
sakshi11101 <sakshi11101@gmail.com>
2021-06-22 19:17:50 +0530

    calculated winner and lift up the state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
+const Board = ({ board, handleSquareClick }) => {
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [isXNext, setIsXNext] = useState(false);
 
-  const handleSquareClick = position => {
-    if (board[position]) {
-      return;
-    }
+  // const handleSquareClick = position => {
+  //   if (board[position]) {
+  //     return;
+  //   }
 
-    setBoard(prev => {
-      return prev.map((square, pos) => {
-        if (pos === position) {
-          return isXNext ? 'X' : 'O';
-        }
+  //   setBoard(prev => {
+  //     return prev.map((square, pos) => {
+  //       if (pos === position) {
+  //         return isXNext ? 'X' : 'O';
+  //       }
 
-        return square;
-      });
-    });
+  //       return square;
+  //     });
+  //   });
 
-    setIsXNext(prev => !prev);
-  };
+  //   setIsXNext(prev => !prev);
+  // };
 
   const renderSquare = position => {
     return (
