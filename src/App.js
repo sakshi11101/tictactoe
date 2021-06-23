be3fed217dfa586071f35e79e9dee8eddc282414
sakshi11101 <sakshi11101@gmail.com>
2021-06-23 14:31:04 +0530

    highlight winning squares and add start new game button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
 import History from './components/History';
+import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './helpers';
 
 import './styles/root.scss';
 
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
+
 const App = () => {
-  const [history, setHistory] = useState([{board : Array(9).fill(null), isXNext : true}, ]);
+  const [history, setHistory] = useState(NEW_GAME);
 
   const [currentMove, setCurrentMove] = useState(0);
 
@@ -14,11 +17,7 @@ const App = () => {
 
   // const [isXNext, setIsXNext] = useState(false);
 
-  const winner = calculateWinner(current.board);
-
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next Player is ${current.isXNext ? 'X' : 'O'}`;
+  const { winner, winningSquares } = calculateWinner(current.board);
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -26,9 +25,9 @@ const App = () => {
     }
 
     setHistory(prev => {
-       const last = prev[prev.length-1]
+      const last = prev[prev.length - 1];
 
-     const newBoard = last.board.map((square, pos) => {
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
           return last.isXNext ? 'X' : 'O';
         }
@@ -36,22 +35,34 @@ const App = () => {
         return square;
       });
 
-      return prev.concat({board : newBoard, isXNext : !last.isXNext});
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
 
     setCurrentMove(prev => prev + 1);
   };
 
-  const moveTo = (move) => {
+  const moveTo = move => {
     setCurrentMove(move);
-  }
+  };
+
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2> {message} </h2>
-      <Board board={current.board} handleSquareClick={handleSquareClick} />
-      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
+      <StatusMessage winner={winner} current={current} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+      <button type="button" onClick={onNewGame}>
+        Start New Game
+      </button>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({ board, handleSquareClick }) => {
-  // const [board, setBoard] = useState(Array(9).fill(null));
-  // const [isXNext, setIsXNext] = useState(false);
-
-  // const handleSquareClick = position => {
-  //   if (board[position]) {
-  //     return;
-  //   }
-
-  //   setBoard(prev => {
-  //     return prev.map((square, pos) => {
-  //       if (pos === position) {
-  //         return isXNext ? 'X' : 'O';
-  //       }
-
-  //       return square;
-  //     });
-  //   });
-
-  //   setIsXNext(prev => !prev);
-  // };
-
+const Board = ({ board, handleSquareClick, winningSquares }) => {
   const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
+
     return (
       <Square
         value={board[position]}
         onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
       />
     );
   };
